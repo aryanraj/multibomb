8b80d61c4eb5d86c7f9d08a5ac9f12aa24c1f3e0
Aryan Raj <aryan.raj15604@gmail.com>
2014-12-07 20:31:29 +0530

    Added config file

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express=require('express.io'),
-	app=express().http().io();
+	app=express().http().io(),
+	createGame = require(__dirname+'/src/create_game.js'),
+	cnf = require(__dirname+'/config/levels')
 
 app.use(express.static(__dirname+'/src'));
 app.use(express.static(__dirname+'/public'));
 app.use(express.static(__dirname+'/bower_components'));
+
+app.io.route('done', function(req){
+	var level1 = createGame(cnf['1'],[req.socket.id]);
+	level1.images = cnf["images"];
+	req.io.emit("data",level1);
+})
 
 app.get('/', function(req,res){
 	res.sendfile(__dirname+'/public/index.html');
